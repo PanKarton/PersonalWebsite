e34abc475cfa4dce43528524637c593ba7f0469c
arek <jack-swad@wp.pl>
2023-08-25 16:52:55 +0100

    test

--- src/modules/SkillsSection/SkillsSection.tsx
+++ src/modules/SkillsSection/SkillsSection.tsx
@@ -1,3 +1,3 @@
 import { MainPageSection } from '@/components/molecules/MainPageSection/MainPageSection';
 
-export const SkillsSection = () => <MainPageSection>My skills</MainPageSection>;
+export const SkillsSection = () => <MainPageSection>My techstack</MainPageSection>;
