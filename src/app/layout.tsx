8e4a4de30abb39acff186687a0340c7746254920
arek <jack-swad@wp.pl>
2023-08-30 19:11:39 +0100

    style/removed scroll bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,35 @@
+'use client';
+
 import { GlobalStyles } from '@/assets/globalStyles';
 import StyledComponentsRegistry from '@/lib/registry';
 import { AppProvider } from '@/providers/AppProvider';
 import { Courier_Prime } from 'next/font/google';
+import styled from 'styled-components';
 
 const courierPrime = Courier_Prime({
   weight: '400',
   subsets: ['latin'],
 });
 
+export const StyledBody = styled.body`
+  &::-webkit-scrollbar {
+    /* Hide scrollbar for Chrome, Safari and Opera */
+    display: none;
+  }
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+`;
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <AppProvider>
       <html>
-        <body suppressHydrationWarning={true} className={courierPrime.className}>
+        <StyledBody suppressHydrationWarning={true} className={courierPrime.className}>
           <StyledComponentsRegistry>
             <GlobalStyles />
             {children}
           </StyledComponentsRegistry>
-        </body>
+        </StyledBody>
       </html>
     </AppProvider>
   );
