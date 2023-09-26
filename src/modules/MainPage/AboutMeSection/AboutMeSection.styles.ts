7c23e9201a075ba84bfadbdbfd47839d8483dab7
arek <jack-swad@wp.pl>
2023-09-26 15:59:38 +0100

    feat/added html structure for section

--- src/modules/MainPage/AboutMeSection/AboutMeSection.styles.ts
+++ src/modules/MainPage/AboutMeSection/AboutMeSection.styles.ts
@@ -1,30 +1,31 @@
 import styled from 'styled-components';
 
 export const FlexWrapper = styled.div`
-  border: 1px solid blue;
+  /* border: 1px solid blue; */
 
   height: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 2rem;
 
-  padding-block: 3rem;
+  padding-block: 3rem 5rem;
   @media screen and (min-width: 50rem) {
     justify-content: flex-start;
   }
 `;
 export const StyledTextWrapper = styled.div`
-  border: 1px solid red;
-
-  width: calc(100vw - 3rem);
+  /* border: 1px solid red; */
 
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: flex-end;
   gap: 0.5rem;
 
   .paragraph {
     margin-top: 0.75rem;
+    text-align: right;
   }
   a {
     margin-top: 1rem;
@@ -32,6 +33,7 @@ export const StyledTextWrapper = styled.div`
 
   @media screen and (min-width: 37.5rem) {
     max-width: 25rem;
+    align-items: flex-end;
   }
 
   @media screen and (min-width: 56.25rem) {
@@ -40,4 +42,28 @@ export const StyledTextWrapper = styled.div`
   }
 `;
 
-export const StyledImagesWrapper = styled.div``;
+export const StyledImagesWrapper = styled.div`
+  /* border: 1px solid red; */
+  position: relative;
+  width: 100%;
+  aspect-ratio: 0.95;
+
+  .big-img-wrapper {
+    position: absolute;
+    aspect-ratio: 0.92;
+    width: 90%;
+
+    top: 0;
+    right: 0;
+  }
+  .small-img-wrapper {
+    position: absolute;
+    aspect-ratio: 1;
+    bottom: 0;
+    left: 0;
+    width: 45%;
+  }
+  @media screen and (max-width: 37.5rem) {
+    display: none;
+  }
+`;
--- src/modules/MainPage/AboutMeSection/AboutMeSection.tsx
+++ src/modules/MainPage/AboutMeSection/AboutMeSection.tsx
@@ -3,6 +3,9 @@ import { FrameLink } from '@/components/atoms/FrameLink/FrameLink';
 import { SectionHeading } from '@/components/atoms/SectionHeading/SectionHeading';
 import { SectionParagraph } from '@/components/atoms/SectionParagraph/SectionParagraph';
 import { MainPageSection } from '@/components/molecules/MainPageSection/MainPageSection';
+import Image from 'next/image';
+import bigImage from 'public/images/about-me-img-big.png';
+import smallImage from 'public/images/about-me-img-small.png';
 import { FlexWrapper, StyledTextWrapper, StyledImagesWrapper } from './AboutMeSection.styles';
 
 export const AboutMeSection = () => (
@@ -16,7 +19,14 @@ export const AboutMeSection = () => (
         </SectionParagraph>
         <FrameLink href={'/about-me'}>Show more</FrameLink>
       </StyledTextWrapper>
-      <StyledImagesWrapper></StyledImagesWrapper>
+      <StyledImagesWrapper>
+        <div className="big-img-wrapper">
+          <Image src={bigImage} alt="me sitting with laptop" fill />
+        </div>
+        <div className="small-img-wrapper">
+          <Image src={smallImage} alt="me drinking coffee" fill />
+        </div>
+      </StyledImagesWrapper>
     </FlexWrapper>
   </MainPageSection>
 );
