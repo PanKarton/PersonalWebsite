c7a68c917372ffa240495f4f9db5fb97dda1808b
arek <jack-swad@wp.pl>
2024-02-04 21:22:38 +0000

    feat/added route handler called project with GET function calling mongoDB and returning projects data

--- src/helpers/buildURL.ts
+++ src/helpers/buildURL.ts
@@ -1,3 +1,9 @@
 import { getEnvVariable } from './getEnvVariable';
 
-export const buildURL = (path: string) => getEnvVariable(process.env.PAGE_URL) + path;
+export const buildURL = (path?: string | undefined) => {
+  const pageUrl =
+    process.env.NODE_ENV === 'production'
+      ? getEnvVariable(process.env.NEXT_PUBLIC_PAGE_URL_PROD)
+      : getEnvVariable(process.env.NEXT_PUBLIC_PAGE_URL_DEV);
+  return !path ? pageUrl + path : pageUrl;
+};
