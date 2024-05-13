179db803ff59c0963b89810bc395f56a43bf555b
Arkadiusz <jack-swad@wp.pl>
2024-05-13 21:07:38 +0200

    refactor/removed code making scrollbar invisible in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import { GlobalStyles } from '@/assets/globalStyles';
 import StyledComponentsRegistry from '@/lib/registry';
 import { AppProvider } from '@/providers/AppProvider';
 import { Courier_Prime } from 'next/font/google';
-import styled from 'styled-components';
 import '../assets/globalStyles.css';
 
 const courierPrime = Courier_Prime({
@@ -12,25 +10,15 @@ const courierPrime = Courier_Prime({
   subsets: ['latin'],
 });
 
-export const StyledBody = styled.body`
-  &::-webkit-scrollbar {
-    /* Hide scrollbar for Chrome, Safari and Opera */
-    display: none;
-  }
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
-`;
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <AppProvider>
       <html style={{ position: 'relative' }}>
-        <StyledBody suppressHydrationWarning={true} className={`${courierPrime.className}`}>
+        <body suppressHydrationWarning={true} className={`${courierPrime.className}`}>
           <StyledComponentsRegistry>
-            <GlobalStyles />
             <>{children}</>
           </StyledComponentsRegistry>
-        </StyledBody>
+        </body>
       </html>
     </AppProvider>
   );
